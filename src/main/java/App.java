1bdfc948b4055dce0504ca7fa5774efd562cce28
Olivice <olivier.vicente95@gmail.com>
2016-11-21 15:43:27 +0100

    Add basic JavaFX window structure

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,29 @@
-
+import javafx.application.Application;
+import javafx.stage.Stage;
+import view.Window;
 
 /**
  * Created by Olivice on 18/11/2016.
  */
-public class App {
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Window window = new Window(primaryStage);
+        window.render();
+    }
 }
--- src/main/java/view/Window.java
+++ src/main/java/view/Window.java
@@ -1,7 +1,81 @@
 package view;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 /**
  * Created by Olivice on 18/11/2016.
  */
 public class Window {
+
+    private Stage primaryStage;
+
+    public Window() {
+    }
+
+    public Window(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public void render() throws Exception {
+            // définit la largeur et la hauteur de la fenêtre
+            // en pixels, le (0, 0) se situe en haut à gauche de la fenêtre
+            primaryStage.setWidth(800);
+            primaryStage.setHeight(600);
+            // met un titre dans la fenêtre
+            primaryStage.setTitle("Joli décor!");
+
+            // la racine du sceneGraph est le root
+            Group root = new Group();
+            Scene scene = new Scene(root);
+            scene.setFill(Color.SKYBLUE);
+
+            // création du soleil
+            Circle sun = new Circle(60, Color.web("yellow", 0.8));
+            sun.setCenterX(600);
+            sun.setCenterY(100);
+
+            // création du sol
+            Rectangle ground = new Rectangle(0, 400, 800, 200);
+            ground.setFill(Color.GREEN);
+
+            // création d'un élément plus complexe, le panneau
+            Group sign = new Group();
+            sign.setTranslateX(150);
+            sign.setTranslateY(200);
+            // Attention les coordonnées sont celles du panneau, pas de la scène
+            Text text = new Text(10, 30, "Hello world!");
+            text.setFont(new Font(80));
+            text.setFill(Color.WHITE);
+            // le repère utilisé est celui du panneau
+            Rectangle panel = new Rectangle( 0, -50, 500, 110);
+            panel.setFill(Color.DARKBLUE);
+            // composer l'élément plus complexe
+            sign.getChildren().add(panel);
+            sign.getChildren().add(text);
+
+            // ajout de tous les éléments de la scène
+            root.getChildren().add(sun);
+            root.getChildren().add(ground);
+            root.getChildren().add(sign);
+
+            // ajout de la scène sur l'estrade
+            primaryStage.setScene(scene);
+            // ouvrir le rideau
+            primaryStage.show();
+    }
 }
