ea245dbd5115f860ae608c5ee61fd5bf5b87f8b8
Tom Pitel <pitel.tom@gmail.com>
2016-11-22 12:01:35 +0100

    Edit Dijkstra

--- src/main/java/model/Graph.java
+++ src/main/java/model/Graph.java
@@ -1,7 +1,11 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+
+import Dijkstra.Dijkstra;
 
 /**
  * Created by Olivice on 18/11/2016.
@@ -12,6 +16,34 @@ public class Graph {
     private Map<Integer, CrossingPoint> crossingPoints;
 
     public Graph() {
+    	
+    	List<Path> paths = new ArrayList<Path>();
+		Dijkstra dijkstra = new Dijkstra(p);
+		
+		for(Map.Entry<Integer,CrossingPoint> origin : t.getCrossingPoints().entrySet())
+		{
+			dijkstra.execute(origin.getValue().getIntersection());
+			
+			for(Map.Entry<Integer,CrossingPoint> destination : t.getCrossingPoints().entrySet())
+			{
+				if(!destination.equals(origin))
+				{
+					LinkedList<Intersection> listinter = dijkstra.getPath(destination.getValue().getIntersection());
+					List<Section> sections = new ArrayList<Section>();
+					int length = 0;
+					for(int i=0 ; i<listinter.size()-1; i++)
+					{
+						Section toAdd = listinter.get(i).getSectionTo(listinter.get(i+1));
+						sections.add(toAdd);
+						length += toAdd.getLength();
+					}
+					
+					paths.add(new Path(sections,length));
+				}
+			}
+		}
+		
+		this.paths=paths;
     }
 
     public Graph(List<Path> paths, Map<Integer, CrossingPoint> crossingPoints) {
