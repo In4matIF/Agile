0d3f2515e8958a1ea79fca585e0b03286e17065f
Louis <Louis@japan-laptop>
2016-11-24 18:53:04 +0100

    Ajout du package test et de la classe PlanTest

---
+++ src/main/java/test/PlanTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Plan;
+
+public class PlanTest {
+
+	private File xmlPlan;
+	private Plan p;
+	
+	@Before
+	public void setUp() throws Exception {
+		 xmlPlan = new File("src/main/resources/xml/plan5x5.xml");
+		 p = new Plan(xmlPlan);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPlanNonVide() {
+		assertFalse((p.getIntersections().size()==0)||(p.getSections().size()==0));
+	}
+}
