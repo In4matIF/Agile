34751b3405226bd5d2ea59dd267358c881597070
Louis <Louis@10.43.4.54>
2016-11-25 08:23:46 +0100

    Warehouse colored in green

--- src/main/java/view/Window.java
+++ src/main/java/view/Window.java
@@ -234,7 +234,7 @@ public class Window {
         float widthRatio = 0.95f;//(float)CANVAS_WIDTH/(float)SCENE_WIDTH;
         float heightRatio = (float)CANVAS_HEIGHT/(float)SCENE_HEIGHT;
 
-        // crﾃｩation d'une intersection
+        // cr�ｾ�ｽｩation d'une intersection
         gc.setFill(Color.DARKBLUE);
         gc.setStroke(Color.DARKBLUE);
         gc.setLineWidth(3);
@@ -277,21 +277,29 @@ public class Window {
     public void renderLivraison(TextArea filler1, Canvas planCanvas, GraphicsContext gc) throws Exception {
         filler1.setText("");
         
-        gc.setFill(Color.DARKRED);
         gc.setStroke(Color.DARKRED);
         
         float widthRatio = 0.95f;//(float)CANVAS_WIDTH/(float)SCENE_WIDTH;
         float heightRatio = (float)CANVAS_HEIGHT/(float)SCENE_HEIGHT;
-        
         
         tour.getCrossingPoints().forEach(
                 (integer, crossingPoint) -> {
                     String deliverys = filler1.getText() + "Livraison : " + integer + "\r\n";
                     filler1.setText(deliverys);
                     
+
                     float x = crossingPoint.getIntersection().getX()*widthRatio;
                     float y = crossingPoint.getIntersection().getY()*heightRatio;
                     int radius = 8;
+                    
+                    if(tour.getIdWarehouse()==integer)
+                    {
+                    	gc.setFill(Color.GREEN);
+                    }
+                    else
+                    {
+                    gc.setFill(Color.DARKRED);
+                    }   
                     
                     gc.fillOval(x-radius,y-radius,2*radius,2*radius);
                     
