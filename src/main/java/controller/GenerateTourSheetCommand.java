94e889ccb43bd1b6d30a951ffb6ea5a19d3e4109
vbonnet <vbonnet@if501-204-07.insa-lyon.fr>
2016-11-29 11:58:06 +0100

    Generation of Tour Sheet

--- src/main/java/controller/GenerateTourSheetCommand.java
+++ src/main/java/controller/GenerateTourSheetCommand.java
@@ -1,5 +1,14 @@
 package controller;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import model.Section;
+import view.Window;
+
 /**
  * Commande liée à la génération d'une feuille de route
  */
@@ -10,6 +19,66 @@ public class GenerateTourSheetCommand implements Command {
 
     @Override
     public boolean doCommand() {
+    	String tourMessage = "";
+    	Map<Integer, Section> sections = Window.tour.getSections();
+    	tourMessage += "Prenez la rue "+ sections.get(0).getStreet() + "\r\n";
+    	for(int i = 1; i < sections.size(); i++)
+    	{
+    	    float x1 = sections.get(i-1).getDestination().getX() - sections.get(i-1).getOrigin().getX();
+    	    float y1 = sections.get(i-1).getDestination().getY() - sections.get(i-1).getOrigin().getY();
+    	    float x2 = sections.get(i).getDestination().getX() - sections.get(i).getOrigin().getX();
+    	    float y2 = sections.get(i).getDestination().getY() - sections.get(i).getOrigin().getY();
+    	    
+    	    double angle1 = Math.atan2(x1, y1);
+    	    double angle2 = Math.atan2(x2, y2);
+    	    
+    	    double deltaA = angle1 - angle2;
+    	    
+    	    if(deltaA < Math.toRadians(-45))
+    	    {
+    	    	tourMessage += "Tournez a gauche vers la rue "+ sections.get(i).getStreet() + "\r\n";
+    	    }
+    	    
+    	    else if(deltaA < Math.toRadians(-20))
+    	    {
+    	    	tourMessage += "Tournez legerement à gauche vers la rue "+ sections.get(i).getStreet() + "\r\n";
+    	    }
+    	    
+    	    else if(deltaA > Math.toRadians(45))
+    	    {
+    	    	tourMessage += "Tournez a droite vers la rue "+ sections.get(i).getStreet() + "\r\n";
+    	    }
+    	    
+    	    else if(deltaA > Math.toRadians(20))
+    	    {
+    	    	tourMessage += "Tournez legerement à droite vers la rue "+ sections.get(i).getStreet() + "\r\n";
+    	    }
+    	    
+    	    else
+    	    {
+    	    	tourMessage += "Continuez tour droit vers la rue "+ sections.get(i).getStreet() + "\r\n";
+    	    }
+    	}
+    	
+    	File output = new File("FeuilleDeRoute.txt");
+		try {
+			FileWriter fw = new FileWriter(output);
+			{
+				try {
+					fw.write(tourMessage);
+				} catch (IOException e) {
+					System.out.println("Erreur lors de la lecture des intersections : " + e.getMessage());
+					e.printStackTrace();
+					fw.close();
+					return false;
+				}
+			}
+			fw.close();
+		} catch (IOException e) {
+			System.out.println("Erreur lors de la lecture : " + e.getMessage());
+			return false;
+		}
+	
         return true;
     }
 }
--- src/main/java/view/Window.java
+++ src/main/java/view/Window.java
@@ -258,7 +258,7 @@ public class Window {
 
 		feuilleBtn.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent feiulleEvent) {
-				File output = new File("FeuilleDeRoute.txt");
+				/*File output = new File("FeuilleDeRoute.txt");
 				try {
 					FileWriter fw = new FileWriter(output);
 					{
@@ -272,7 +272,8 @@ public class Window {
 					fw.close();
 				} catch (IOException e) {
 					System.out.println("Erreur lors de la lecture : " + e.getMessage());
-				}
+				}*/
+				controller.generateTourSheet();
 			}
 		});
 
